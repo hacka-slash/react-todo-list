9319bc237eb6b9b313f5103b9cfc55a85b8fea18
hacka-slash <placido.hoff@gmail.com>
2020-04-28 08:47:13 -0400

    first to-do tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './components/ListItems.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    //Must bind the proper 'context' to be able to use 'this' within the functions:
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem: {
+        text: e.target.value, 
+        key: Date.now()
+      }
+    })
+  }
+     
+
+  addItem(e){
+    //prevents the default effect of having the page reload on form Submit
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem !== ""){
+      const newItems = [...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+    console.log(newItem)
+  }
+
+  render() {
+    return(
+    <h1>
+      <div className="App"> 
+      <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Text"
+          value={this.state.currentItem.text}
+          onChange={this.handleInput} />
+          <button type="submit">Add</button>
+        </form>
       </header>
-    </div>
-  );
+      <ListItems items={this.state.items} />
+      </div>
+    </h1>
+    )
+  };
 }
 
 export default App;
